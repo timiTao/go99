124a2957886762e6bfd64ee2776cb45f0cb29214
TimiTao <kunicki.tomasz@gmail.com>
2019-06-22 08:56:34 +0200

    add partial solution to p90

--- miscellaneous/p90.go
+++ miscellaneous/p90.go
@@ -1,5 +1,29 @@
 package miscellaneous
 
+import "math"
+
 func IsCorrectEightQueensProblemSolution(solution [8]int) bool {
-	return false
+	for i := 0; i < 8; i++ {
+		if queensCollision(solution, solution[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func queensCollision(solution [8]int, key int) bool {
+	for i := 0; i < 8; i++ {
+		if i == key {
+			continue
+		}
+		valueI := solution[i]
+		valueKey := solution[key]
+
+		modValue, _ := math.Modf(float64(valueI) - float64(valueKey))
+		modKey, _ := math.Modf(float64(i) - float64(key))
+		if modValue == modKey {
+			return false
+		}
+	}
+	return true
 }
