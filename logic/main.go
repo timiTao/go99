5182db523664e3beaa81c01ae84ea7d38243d3d5
TimiTao <kunicki.tomasz@gmail.com>
2019-07-13 11:43:24 +0200

    p46 add new functions

--- logic/main.go
+++ logic/main.go
@@ -4,3 +4,15 @@ package logic
 func and(a bool, b bool) bool {
 	return a && b
 }
+func or(a bool, b bool) bool {
+	return a || b
+}
+func nand(a bool, b bool) bool {
+	return !and(a, b)
+}
+func nor(a bool, b bool) bool {
+	return !or(a, b)
+}
+func xor(a bool, b bool) bool {
+	return (a || b) && !(a && b)
+}
--- logic/p46_test.go
+++ logic/p46_test.go
@@ -3,7 +3,116 @@ package logic
 import "testing"
 
 func TestAnd(t *testing.T) {
-	if !and(true, true) {
-		t.Error("Expected to be true")
-	}
+	t.Run("all true", func(t *testing.T) {
+		if !and(true, true) {
+			t.Error("Expected to be true")
+		}
+	})
+	t.Run("true/false", func(t *testing.T) {
+		if and(true, false) {
+			t.Error("Expected to be false")
+		}
+	})
+	t.Run("false/true", func(t *testing.T) {
+		if and(true, false) {
+			t.Error("Expected to be false")
+		}
+	})
+	t.Run("all false", func(t *testing.T) {
+		if and(false, false) {
+			t.Error("Expected to be false")
+		}
+	})
+}
+
+func TestOr(t *testing.T) {
+	t.Run("all true", func(t *testing.T) {
+		if !or(true, true) {
+			t.Error("Expected to be true")
+		}
+	})
+	t.Run("true/false", func(t *testing.T) {
+		if !or(true, false) {
+			t.Error("Expected to be true")
+		}
+	})
+	t.Run("false/true", func(t *testing.T) {
+		if !or(true, false) {
+			t.Error("Expected to be true")
+		}
+	})
+	t.Run("all false", func(t *testing.T) {
+		if or(false, false) {
+			t.Error("Expected to be false")
+		}
+	})
+}
+
+func TestNand(t *testing.T) {
+	t.Run("all true", func(t *testing.T) {
+		if nand(true, true) {
+			t.Error("Expected to be false")
+		}
+	})
+	t.Run("true/false", func(t *testing.T) {
+		if !nand(true, false) {
+			t.Error("Expected to be true")
+		}
+	})
+	t.Run("false/true", func(t *testing.T) {
+		if !nand(true, false) {
+			t.Error("Expected to be true")
+		}
+	})
+	t.Run("all false", func(t *testing.T) {
+		if !nand(false, false) {
+			t.Error("Expected to be true")
+		}
+	})
+}
+
+func TestNor(t *testing.T) {
+	t.Run("all true", func(t *testing.T) {
+		if nor(true, true) {
+			t.Error("Expected to be false")
+		}
+	})
+	t.Run("true/false", func(t *testing.T) {
+		if nor(true, false) {
+			t.Error("Expected to be false")
+		}
+	})
+	t.Run("false/true", func(t *testing.T) {
+		if nor(true, false) {
+			t.Error("Expected to be false")
+		}
+	})
+	t.Run("all false", func(t *testing.T) {
+		if !nor(false, false) {
+			t.Error("Expected to be true")
+		}
+	})
+}
+
+func TestXor(t *testing.T) {
+	t.Run("all true", func(t *testing.T) {
+		if xor(true, true) {
+			t.Error("Expected to be false")
+		}
+	})
+	t.Run("true/false", func(t *testing.T) {
+		if !xor(true, false) {
+			t.Error("Expected to be true")
+		}
+	})
+	t.Run("false/true", func(t *testing.T) {
+		if !xor(true, false) {
+			t.Error("Expected to be true")
+		}
+	})
+	t.Run("all false", func(t *testing.T) {
+		if xor(false, false) {
+			t.Error("Expected to be false")
+		}
+	})
 }
